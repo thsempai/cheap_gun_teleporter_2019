71667cf84c64f61344b4fc55a68eab1423225dc8
Thomas Stassin <thomas@panopticgame.com>
2019-06-05 16:17:05 +0200

    first scene with FPS and gun

---
+++ Cheap Gun Telepoter/Assets/Scripts/StartForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartForce : MonoBehaviour
+{
+    public float force = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 forceVector = transform.forward * force;
+        GetComponent<Rigidbody>().AddForce(forceVector);
+        Destroy(gameObject, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
